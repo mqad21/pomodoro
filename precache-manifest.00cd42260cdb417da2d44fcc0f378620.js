499060af232e60a0dd3622ef26d10aaaf31338bc
Muhammad Qadri <221709860@stis.ac.id>
2020-07-03 14:43:08 +0700

    Updates

---
+++ precache-manifest.00cd42260cdb417da2d44fcc0f378620.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "635f62401813018a4bc2673fd3dee66f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2fafc8aa1a6bb9630b7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4ad05961998d95247b9a",
+    "url": "/static/js/2.cd8b3c57.chunk.js"
+  },
+  {
+    "revision": "2142ab953a70f3d6b728c052b5d2e704",
+    "url": "/static/js/2.cd8b3c57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b7849fbb5983318e847",
+    "url": "/static/js/3.464d5028.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/static/js/3.464d5028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2fafc8aa1a6bb9630b7",
+    "url": "/static/js/main.54976336.chunk.js"
+  },
+  {
+    "revision": "ce178ba97f30356cbdfa",
+    "url": "/static/js/runtime-main.76e556a2.js"
+  },
+  {
+    "revision": "a292996ee8d14cbfcd8305ceeb6e3ac1",
+    "url": "/static/media/alarm.a292996e.mp3"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,216 @@
+import workbox from 'https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js';
+
+const precacheManifest = [
+  {
+    "url": "asset-manifest.json",
+    "revision": "d99ccd9a4f44762cb1993652a9535cd7"
+  },
+  {
+    "url": "assets/no-data.png",
+    "revision": "72a4dddcdff5cddb69512c332ee076af"
+  },
+  {
+    "url": "index.html",
+    "revision": "635f62401813018a4bc2673fd3dee66f"
+  },
+  {
+    "url": "logo192.png",
+    "revision": "33dbdd0177549353eeeb785d02c294af"
+  },
+  {
+    "url": "logo512.png",
+    "revision": "917515db74ea8d1aee6a246cfbcc0b45"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "46fa364aeb97b861225fa8fe4e2532d2"
+  },
+  {
+    "url": "precache-manifest.00cd42260cdb417da2d44fcc0f378620.js",
+    "revision": "00cd42260cdb417da2d44fcc0f378620"
+  },
+  {
+    "url": "robots.txt",
+    "revision": "fa1ded1ed7c11438a9b0385b1e112850"
+  },
+  {
+    "url": "service-worker.js",
+    "revision": "7490c8bd7671214e3d18a9441d5bb8cb"
+  },
+  {
+    "url": "static/css/main.5f361e03.chunk.css",
+    "revision": "1472653e7cd5f55fc8eabda93770382e"
+  },
+  {
+    "url": "static/js/2.cd8b3c57.chunk.js",
+    "revision": "2982dac1394d4aa57891563b195708d2"
+  },
+  {
+    "url": "static/js/2.cd8b3c57.chunk.js.LICENSE.txt",
+    "revision": "2142ab953a70f3d6b728c052b5d2e704"
+  },
+  {
+    "url": "static/js/3.464d5028.chunk.js",
+    "revision": "e7ae636f2ba65c21b280b52af6c362a9"
+  },
+  {
+    "url": "static/js/3.464d5028.chunk.js.LICENSE.txt",
+    "revision": "287e48aeac3564a8b2b5685a67fb9216"
+  },
+  {
+    "url": "static/js/main.54976336.chunk.js",
+    "revision": "96554467f8d23de1d811e7273a7cc15f"
+  },
+  {
+    "url": "static/js/runtime-main.76e556a2.js",
+    "revision": "e3b851330325c62387fa802acafd3600"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-background-sync.dev.js",
+    "revision": "5446355eef3aa184b5b6eebfcd81f8d9"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-background-sync.prod.js",
+    "revision": "1ffcc362312a9e8ef4e28280ace2a1bd"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-broadcast-update.dev.js",
+    "revision": "0508d13784c9b0549663f40e3fe55d37"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-broadcast-update.prod.js",
+    "revision": "ee27c0fdc836f6a2dc656b25a680f9e4"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-cacheable-response.dev.js",
+    "revision": "ecba3679d285394f1c9e219681662721"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-cacheable-response.prod.js",
+    "revision": "a38e8afa80070ec9dff5dc2fb116f1c2"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-core.dev.js",
+    "revision": "2912182ccc99b017a8c36802cf9d983f"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-core.prod.js",
+    "revision": "5d14d8267f65030735589e4b664ee3bf"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-expiration.dev.js",
+    "revision": "43c236fe62480f042c35e8b898ca3367"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-expiration.prod.js",
+    "revision": "a767f3bbd2773a0bea34ff841b51ab64"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-navigation-preload.dev.js",
+    "revision": "a8f30e409f7037906053acec7d709beb"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-navigation-preload.prod.js",
+    "revision": "e2b19a3eda50f48ce7fc48640a523353"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-offline-ga.dev.js",
+    "revision": "3fba0947d12d42834b81499fafc76e82"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-offline-ga.prod.js",
+    "revision": "6af4fb51a5249c4e0ed7bc61ed59836d"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-precaching.dev.js",
+    "revision": "8fbbefcd70c998a3cd35f02e6a8ac4e7"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-precaching.prod.js",
+    "revision": "e8f5c57430ec7c448d30015ff4bd5896"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-range-requests.dev.js",
+    "revision": "0f15c57cf5c75cc72b6f23ad28a941d1"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-range-requests.prod.js",
+    "revision": "97c430406d13f4b91c805594ef351261"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-routing.dev.js",
+    "revision": "471b8e0f45e6e5e679d04f60c6466e7f"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-routing.prod.js",
+    "revision": "d3fa76a1c38649d596b1d2ffaf398128"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-strategies.dev.js",
+    "revision": "d1c19737e82e2f6bd567aaf384683174"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-strategies.prod.js",
+    "revision": "6033181992f0bc562ab1ef5f9ba34697"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-streams.dev.js",
+    "revision": "eed9eb6f7b0672c45db5408d05efe9b9"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-streams.prod.js",
+    "revision": "4407a13523f1fa1064f616e9047b6148"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-sw.js",
+    "revision": "6e1e47d706556eac8524f396e785d4bb"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-window.dev.umd.js",
+    "revision": "c17834573a1b48bb8cf33b12128bdae9"
+  },
+  {
+    "url": "workbox-v4.3.1/workbox-window.prod.umd.js",
+    "revision": "c65238721ed1187cf832e51a9e34724a"
+  }
+];
+
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(precacheManifest);
+
+workbox.routing.registerRoute(
+    /.*.(?:js|css|ttf|otf|mp3)$/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'pomodoro-assets'
+    }),
+    'GET');
+
+workbox.routing.registerRoute(
+    /.*.(?:png|jpg|jpeg|svg)$/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'pomodoro-images'
+    }),
+    'GET');
+
+workbox.routing.registerRoute(
+    /^https:\/\/fonts\.googleapis\.com/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'google-fonts-stylesheets',
+    })
+);
+
+workbox.routing.registerRoute(
+    /^https:\/\/fonts\.gstatic\.com/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'google-fonts-webfonts',
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200],
+            }),
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+                maxEntries: 30,
+            }),
+        ],
+    })
+);
